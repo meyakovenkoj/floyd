c6a5898e354e651c2fa3d17840c35dc4cb3a6b5b
Ivan <meyakovenkoj@yandex.ru>
2018-12-20 21:27:10 +0300

    Initial Commit

---
+++ Lab3-graph/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab3-graph
+//
+//  Created by Ivan Yakovenko on 20/12/2018.
+//  Copyright © 2018 Ivan Yakovenko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
